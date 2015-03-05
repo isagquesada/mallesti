b7285c306c4bafa1ceda028f12367de215cd0ec3
isabel <isgon22@gmail.com>
2015-03-05 13:50:33 +0100

    borrar customer

--- app/assets/javascripts/customer.js
+++ app/assets/javascripts/customer.js
@@ -1,5 +1,5 @@
 (function(){
-  var app = angular.module('mallesti-customer', []);
+  var app = angular.module('mallesti-customer', ['ui.router']);
 
   app.directive('customerList', function(){
   return {
@@ -18,8 +18,29 @@
     .success(function(data){
       scope.customers = data.customers;
     })
+  
+  scope.removeCustomer = function (customer){
+
+  if(confirm("Estas seguro de borrar cliente" + customer.name + "?")){
+   $http.delete("/customers/" + customer.id + ".json")
+    .success(function(){
+      //busco el indice del array que contiene el objeto customer
+      var index = scope.customers.indexOf(customer);
+      //borra la posicion index del array
+      scope.customers.splice(index, 1);
+    })
+    }
+  };
  }]);
 
- 
+ app.controller('CustomerIdController', ['$http', '$state', function($http, $state){
+  var scope = this;
+  scope.customer = [];
+
+  $http.get("/customers/" + $state.params.id + ".json") //este id es el que ponermos en el state que en la url (customers/:id) 
+    .success(function(data){
+      scope.customer = data.customer;
+    })
+  }]);
 
 })();
